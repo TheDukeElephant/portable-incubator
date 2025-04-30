f5f91704ee5de8be95b5c9d8c9828a126c9e7bb0
G. M. Ursone <128614215+TheDukeElephant@users.noreply.github.com>
2025-04-30 13:29:23 +0200

    no debug mode after all

--- run.py
+++ run.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     # Note: Running with debug=True is convenient for development,
     # but should be disabled in a production environment.
     # host='0.0.0.0' makes the server accessible on your network.
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False, use_reloader=False)
